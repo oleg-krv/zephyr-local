hillLang/zephyr

x86: add ability for SoC to add MMU regions
The SoCs usually have devices that are accessed through MMIO.
This requires the corresponding regions to be marked readable
and writable in the MMU or else accesses will result in page
faults.

This adds a function which can be implemented in the SoC code to
specify those pages to be added to MMU.

Signed-off-by: Daniel Leung <daniel.leung@intel.com>

arch/x86/core/x86_mmu.c
@@ -817,6 +817,27 @@ static void add_mmu_region(struct x86_page_tables *ptables,
 	}
 }
 
+
+void z_x86_add_mmu_region(uintptr_t addr, size_t size, u64_t flags,
+			  bool user_table)
+{
+	struct mmu_region rgn = {
+		.address = addr,
+		.size = size,
+		.flags = flags,
+	};
+
+	if (!user_table) {
+		add_mmu_region(&z_x86_kernel_ptables, &rgn, false);
+	} else {
+		add_mmu_region(&USER_PTABLES, &rgn, true);
+	}
+}
+
+void __weak z_x86_soc_add_mmu_regions(void)
+{
+}
+
 /* Called from x86's arch_kernel_init() */
 void z_x86_paging_init(void)
 {
@@ -828,6 +849,8 @@ void z_x86_paging_init(void)
 		add_mmu_region(&z_x86_user_ptables, rgn, true);
 #endif
 	}
+
+	z_x86_soc_add_mmu_regions();
 
 	pages_free = (page_pos - page_pool) / MMU_PAGE_SIZE;
 

oleg-krv/zephyr-local

arch/x86/core/x86_mmu.c
@@ -817,6 +817,25 @@ static void add_mmu_region(struct x86_page_tables *ptables,
 	}
 }
 
+
+void z_x86_add_mmu_region(uintptr_t addr, size_t size, u64_t flags)
+{
+	struct mmu_region rgn = {
+		.address = addr,
+		.size = size,
+		.flags = flags,
+	};
+
+	add_mmu_region(&z_x86_kernel_ptables, &rgn, false);
+#ifdef CONFIG_X86_KPTI
+	add_mmu_region(&z_x86_user_ptables, &rgn, true);
+#endif
+}
+
+void __weak z_x86_soc_add_mmu_regions(void)
+{
+}
+
 /* Called from x86's arch_kernel_init() */
 void z_x86_paging_init(void)
 {
@@ -828,6 +847,8 @@ void z_x86_paging_init(void)
 		add_mmu_region(&z_x86_user_ptables, rgn, true);
 #endif
 	}
+
+	z_x86_soc_add_mmu_regions();
 
 	pages_free = (page_pos - page_pool) / MMU_PAGE_SIZE;
 