4b1bd861827150d5a787043dfff97bd2936abcb9
Krivorot Oleg <krivorot.oleg@gmail.com>
2020-06-25 18:58:52 +0300

    gsm modem ppp SimCom

---
+++ drivers/modem/gsm_ppp_simcom.h
@@ -0,0 +1,24 @@
+//
+// Created by oleg on 25.06.20.
+//
+
+#ifndef STM32_BASE_GSM_PPP_SIMCOM_H
+#define STM32_BASE_GSM_PPP_SIMCOM_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <device.h>
+
+
+__syscall void modem_power_on(void);
+__syscall void modem_power_off(void);
+__syscall void modem_power_reset(void);
+__syscall void modem_always_online(bool enable);
+
+#ifdef __cplusplus
+}
+#endif
+
+#include <syscalls/gsm_ppp_simcom.h>
+#endif //STM32_BASE_GSM_PPP_SIMCOM_H
